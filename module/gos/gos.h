adedaab3034e1ffb62869ba4d1a5d262660bc72b
anu1meha <44045398+anu1meha@users.noreply.github.com>
2019-05-14 19:36:02 +0900

    Update gos.h

--- module/gos/gos.h
+++ module/gos/gos.h
@@ -5,6 +5,10 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/cpumask.h>
+#include <linux/kernel_stat.h>
+#include <linux/tick.h>
+#include <linux/time.h>
 
 #if defined(GOS_SOURCE) || defined(CPU_SOURCE)
 
@@ -37,10 +41,17 @@
 */
 
 #define VCPU_NUM 4
-#define VM_NUM 2
+#define VM_NUM 10 
+#define SYS_CPU_UTIL_THRESHOLD 8500 /* 8500 = 85.00% */
+#define WORK_CONSERVING -1
+#define SLA_GOAL 10000
+#define EXIT_CPU_UTIL 50 /* 0.5% */
+#define PERIOD 100000	/* us, 100ms */
+#define SYS_CPU_MAX_UTIL 9800 /* 98.00% */
+#define INC_DEC_SPEED 4
 
-enum scheduler {none, cpu, io};
-enum sla {b_bw, b_iops, b_lat, c_usg};
+enum gos_type {cpu, ssd, network};
+enum sla {c_usg, b_bw, b_iops, b_lat, n_mincredit, n_maxcredit, n_weight};
 
 /*
    Structure Area
@@ -51,6 +62,8 @@ struct vm_perf {
 	unsigned long bandwidth;	// MB/s
 	unsigned long latency;		// ms
 	unsigned long cpu_usage;	// %
+	unsigned long credit;		// pps
+	unsigned long weight;
 };
 
 struct disk_stat {
@@ -60,45 +73,62 @@ struct disk_stat {
 	unsigned long wait;
 };
 
-struct gos_vm_info {
-	struct task_struct *ts[VCPU_NUM];
-	struct task_group *tg[VCPU_NUM];
-
-	char dev_name[20];
+struct gos_vm_sla {
+	/* 
+	 * SLA info
+	 * SLA : 100.00% = 10000
+	 */
+	unsigned long prev_sla;
+	unsigned long now_sla;
 	char sla_option[10];
-
-	// Target SLA value
 	struct vm_perf sla_target;
-
-	// Status value now
-	struct disk_stat now_io_stat;
-	unsigned long now_cpu_time;
-
-	// Performance value now
-	struct vm_perf now_perf;
-
-	// Prev stat feedback controller
-	long prev_SLA;	// 100.00% == 10000
-	long prev_quota;
-
-	// Now stat for feedback controller
-	long now_SLA;	// 100.00 % == 10000
-	long now_quota;
-
-	unsigned long elapsed_time;
-
-	// SLA category number (b_bw: block bandwidth , b_iops: block iops , b_lat: block latency , c_usg: cpu usage)
 	enum sla sla_type;
 
-	// Target scheduler to control VM's resource
-	enum scheduler control_sched;
+	/* quota info */
+	long prev_quota;
+	long now_quota;
+
+	/* Target type to control VM's resource */
+	enum gos_type control_type;
+
+	/* for vm cpu uage */
+	unsigned long prev_cpu_time;
+	unsigned long now_cpu_time;
+
+	/* sla list */
+	struct list_head sla_list;
+};
+
+
+struct gos_vm_info {
+	/* VM info */
+	char vm_name[20];
+
+	/* sla list head */
+	struct list_head sla_list;
+
+	/* task_struct */
+	struct task_struct *vcpu[VCPU_NUM];
+	struct task_struct *vhost;
+	struct task_struct *iothread;
+
+	/* 
+	 * status value used by other modules which measure
+	 * performance stats.
+	 */
+	char dev_name[20];
+	struct disk_stat now_io_stat;
+	struct vm_perf now_perf;
+	void *priv_data;
 };
 
 /*
    Extern Area
 */
 
-extern void cal_io_SLA_percent(int vm_num);
+extern int add_network_sla(struct gos_vm_info *tmp_vm_info, struct gos_vm_sla *tmp_vm_sla, long long vhost_pid); 
+
+extern void cal_io_SLA_percent(int vm_num, struct gos_vm_sla *curr_sla);
 extern void cal_cpu_SLA(int vm_num);
 
 extern long tg_get_cfs_quota(struct task_group *tg);
